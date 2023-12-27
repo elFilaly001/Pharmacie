7478f54cf2f44f1177a996c6123ad6e97fbde064
El Filaly <elfilalyabdeljalil@gmail.com>
2023-12-27 22:05:10 +0100

    recreated the database

--- DB.sql
+++ DB.sql
@@ -1,44 +1,51 @@
-create table user(
-    user_id int AUTO_INCREMENT primary key,
-    full_name varchar(50),
-    email varchar(50),
-    password varchar(255),
-    user_role enum('admin' , 'patient online'), 
-    Constraint user_role_ck check (user_role in ('admin' , 'patient online'))
+CREATE TABLE user (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(50),
+    email VARCHAR(50),
+    pwd VARCHAR(255),
+    user_role ENUM('admin', 'patient'), 
+    CONSTRAINT user_role_ck CHECK (user_role IN ('admin', 'patient'))
 );
 
-create table sale (
-    sale_id int AUTO_INCREMENT primary key , 
-    sale_number int ,
-    product varchar(255),
-    price float , 
-    sale_plat enum('online' , 'on_site'),
-    Constraint sale_plat_ck check sale_plat in ('online' , 'on_site')
+CREATE TABLE medicine (
+    med_id INT AUTO_INCREMENT PRIMARY KEY ,
+    med_name VARCHAR(50),
+    type VARCHAR(50),
+    description VARCHAR(500),
+    price FLOAT, 
+    img VARCHAR(255)
 );
 
-create table user_sale (
-    user_id int ,
-    sale_id int , 
-    primary key (user_id,sale_id),
-    constraint user_sale_sale_id_fk foreign key (sale_id) references sale(sale_id),
-    constraint user_sale_user_id_fk foreign key (user_id) references user(user_id),
+CREATE TABLE sale_raport (
+    sale_raport_id INT AUTO_INCREMENT PRIMARY KEY,
+    raport_number INT UNIQUE , 
+    description VARCHAR(500),
+    total FLOAT
 );
 
-create table stock (
-    stock_id int AUTO_INCREMENT primary key,
-    product varchar(50),
-    price float , 
-    qte_stock int , 
-    sale_id int,
-    constraint user_sale_sale_id_fk foreign key (sale_id) references sale(sale_id),
+CREATE TABLE sale (
+    sale_id INT AUTO_INCREMENT PRIMARY KEY , 
+    sale_number INT UNIQUE,
+    sale_date DATE, 
+    sale_plat ENUM('online', 'store'),
+    user_id INT,
+    med_id INT,
+    sale_report_id INT,
+    CONSTRAINT sale_plat_ck CHECK (sale_plat IN ('online', 'store')),
+    CONSTRAINT sale_user_id_fk FOREIGN KEY (user_id) REFERENCES user(user_id),
+    CONSTRAINT sale_med_id_fk FOREIGN KEY (med_id) REFERENCES medicine(med_id),
+    CONSTRAINT sale_sale_raport_id_fk FOREIGN KEY (sale_report_id) REFERENCES sale_raport(sale_raport_id)
 );
 
-create table raport (
-    report_id int AUTO_INCREMENT primary key,
-    raport_number int , 
-    description varchar(500),
-    total float , 
-    sale_id int,
-    constraint raport_sale_id_fk foreign key sale_id references sale(sale_id),
+CREATE TABLE stock_raport (
+    stock_raport_id INT AUTO_INCREMENT PRIMARY KEY,
+    raport_number INT UNIQUE,
+    description VARCHAR(500)
 );
 
+CREATE TABLE stock (
+    stock_id INT AUTO_INCREMENT PRIMARY KEY,
+    qte_stock INT ,
+    stock_raport_id INT , 
+    CONSTRAINT stock_stock_raport_id_fk FOREIGN KEY (stock_raport_id) REFERENCES stock_raport(stock_raport_id)
+);
